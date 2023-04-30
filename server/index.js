66b2e203f9e0e07bc0ef2e442e4c679180ca6956
Benson gathu <bensongathu23@gmail.com>
2023-04-30 18:41:01 +0300

    Client fixed

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ io.on("connection", (socket) => {
     console.log(data);
     socket.to(data.room).emit("recieve_message",data)
   })
-
+  
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
